40d3321d1e22f6b9347229402dce6e782c3322bb
dknathalage <dknathalage@gmail.com>
2024-12-08 20:39:10 +1100

    deploy

--- modules/web/src/routes/+layout.svelte
+++ modules/web/src/routes/+layout.svelte
@@ -1,23 +1,22 @@
 <script lang="ts">
-	import { FirebaseApp } from 'sveltefire';
-	import { initializeApp } from 'firebase/app';
-	import { getFirestore } from 'firebase/firestore';
-	import { getAuth } from 'firebase/auth';
+  import { initializeApp } from "firebase/app";
+  import { getFirestore } from "firebase/firestore";
+  import { getAuth } from "firebase/auth";
 
-	// Initialize Firebase
-	const app = initializeApp({
-		apiKey: 'AIzaSyAx9sHjdLMtzz7PqDJN54oNr7aWbT9C1y0',
-		authDomain: 'invoissential.firebaseapp.com',
-		projectId: 'invoissential',
-		storageBucket: 'invoissential.firebasestorage.app',
-		messagingSenderId: '534631897526',
-		appId: '1:534631897526:web:cdf401367da9b847c38917',
-		measurementId: 'G-82PGRS1X26'
-	});
-	const firestore = getFirestore(app);
-	const auth = getAuth(app);
+  let appConfig = {
+    apiKey: "AIzaSyAx9sHjdLMtzz7PqDJN54oNr7aWbT9C1y0",
+    authDomain: "invoissential.firebaseapp.com",
+    projectId: "invoissential",
+    storageBucket: "invoissential.firebasestorage.app",
+    messagingSenderId: "534631897526",
+    appId: "1:534631897526:web:cdf401367da9b847c38917",
+    measurementId: "G-82PGRS1X26",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(appConfig);
+  const firestore = getFirestore(app);
+  const auth = getAuth(app);
 </script>
 
-<FirebaseApp {auth} {firestore}>
-	<slot />
-</FirebaseApp>
+<slot />
--- modules/web/src/routes/+page.svelte
+++ modules/web/src/routes/+page.svelte
@@ -1,20 +1,8 @@
 <script>
-	import { SignedIn, SignedOut, Doc, Collection } from 'sveltefire';
-	import { signInWithPopup } from 'firebase/auth';
-	import { GoogleAuthProvider } from 'firebase/auth';
+  import { signInWithPopup } from "firebase/auth";
+  import { GoogleAuthProvider } from "firebase/auth";
 </script>
 
 <svelte:head>
-	<title>Invoissential</title>
+  <title>Invoissential</title>
 </svelte:head>
-
-<SignedIn let:user let:signOut>
-	<p>Hello {user.displayName}</p>
-	<button on:click={signOut}>Sign Out</button>
-</SignedIn>
-
-<SignedOut let:auth>
-	<button on:click={async () => await signInWithPopup(auth, new GoogleAuthProvider())}>
-		Sign In with Google
-	</button>
-</SignedOut>
