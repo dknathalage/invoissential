3fa84c3a015a9997c8507470ed388c6e05d3d4c8
dknathalage <dknathalage@gmail.com>
2024-12-08 19:48:51 +1100

    remove boilerplate

---
+++ modules/web/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { SignedIn, SignedOut, Doc, Collection } from 'sveltefire';
+	import { signInWithPopup } from 'firebase/auth';
+	import { GoogleAuthProvider } from 'firebase/auth';
+</script>
+
+<svelte:head>
+	<title>Invoissential</title>
+</svelte:head>
+
+<SignedIn let:user let:signOut>
+	<p>Hello {user.displayName}</p>
+	<button on:click={signOut}>Sign Out</button>
+</SignedIn>
+
+<SignedOut let:auth>
+	<button on:click={async () => await signInWithPopup(auth, new GoogleAuthProvider())}>
+		Sign In with Google
+	</button>
+</SignedOut>
