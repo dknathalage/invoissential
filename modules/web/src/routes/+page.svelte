3fb508a66324c2d1acea4133ee87134e4e6f20d3
dknathalage <dknathalage@gmail.com>
2024-12-09 20:16:13 +1100

    remove all assets

--- modules/web/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import { signInWithPopup } from "firebase/auth";
-  import { GoogleAuthProvider } from "firebase/auth";
-</script>
-
-<svelte:head>
-  <title>Invoissential</title>
-</svelte:head>
